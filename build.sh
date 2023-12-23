a63b7f4d8555024654a7236b2326f16c05bb0df6
Pau Fernández <pau.fernandez@upc.edu>
2023-12-23 18:00:41 +0100

    Simplifico

--- build.sh
+++ build.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
+LLVM_VERSION="16"
 
-CLANG="clang++-16"
-LLC="llc-16"
-WASMLD="wasm-ld-16"
-
-${CLANG} -O3 -std=c++20 -Wno-pragma-once-outside-header --target=wasm32 -emit-llvm -c -S all.cc
-${LLC} -O3 -march=wasm32 -filetype=obj all.ll
-${WASMLD} -O3 --allow-undefined --no-entry --export-all -o all.wasm all.o
+clang++-${LLVM_VERSION} -O3 -std=c++20 -Wno-pragma-once-outside-header --target=wasm32 -emit-llvm -c -S all.cc
+llc-${LLVM_VERSION} -O3 -march=wasm32 -filetype=obj all.ll
+wasm-ld-${LLVM_VERSION} -O3 --allow-undefined --no-entry --export-all -o all.wasm all.o
