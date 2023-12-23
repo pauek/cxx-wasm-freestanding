2d7caace42caa1076db3c168248b797b2af01992
Pau Fernández <pau.fernandez@upc.edu>
2023-12-23 18:12:06 +0100

    Indent

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 LLVM_VERSION="16"
 
 clang++-${LLVM_VERSION} -O3 -std=c++20 -Wno-pragma-once-outside-header --target=wasm32 -emit-llvm -c -S all.cc
-llc-${LLVM_VERSION} -O3 -march=wasm32 -filetype=obj all.ll
+llc-${LLVM_VERSION}     -O3 -march=wasm32 -filetype=obj all.ll
 wasm-ld-${LLVM_VERSION} -O3 --allow-undefined --no-entry --export-all -o all.wasm all.o
